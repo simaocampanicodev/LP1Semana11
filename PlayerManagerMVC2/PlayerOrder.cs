612a5da57c65588d0788f6a13ab6680f0b1f0cc6
Simão <simaocampanico.dev@gmail.com>
2025-05-14 22:57:13 +0100

    Project PlayerManagerMVC2 created and files from PlayerManagerMVC duplicated

---
+++ PlayerManagerMVC2/PlayerOrder.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using PlayerManagerMVC2.Models;
+
+namespace PlayerManagerMVC2
+{
+    public enum PlayerOrder
+    {
+        ByScore = 1,
+        ByName = 2,
+        ByNameReverse = 3
+    }
+}
---
+++ PlayerManagerMVC2/Program.cs
@@ -0,0 +1,15 @@
+﻿using PlayerManagerMVC2.Views;
+using PlayerManagerMVC2.Controllers;
+
+namespace PlayerManagerMVC2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            PlayerView view = new PlayerView();
+            PlayerController controller = new PlayerController(view);
+            controller.Start();
+        }
+    }
+}
