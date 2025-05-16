ea03786d26552f6686f85c5b4f04cc8038cb0836
Simão <simaocampanico.dev@gmail.com>
2025-05-16 14:16:52 +0100

    Creating MathDuel project and adding to the solution

---
+++ MathDuel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathDuel
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
